a25dba910f0ab7c12495e2a96ee39d67b655ed2b
JoJinWoo <wls94kr@gmail.com>
2019-10-29 23:23:39 +0900

    init ManagerWriteAction

---
+++ etranger/src/manager/action/ManagerWriteAction.java
@@ -0,0 +1,17 @@
+package manager.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.action.Action;
+import common.vo.ActionForward;
+
+public class ManagerWriteAction implements Action {
+
+	@Override
+	public ActionForward excute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return null;
+	}
+
+}
