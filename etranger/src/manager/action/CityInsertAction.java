28cbac3748869ae2fb849c63f64deb4dcfc57b32
wls94kr <wls94kr@gmail.com>
2019-11-13 17:38:34 +0900

    city추가

--- etranger/src/manager/action/CityInsertAction.java
+++ etranger/src/manager/action/CityInsertAction.java
@@ -7,26 +7,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.action.Action;
 import common.vo.ActionForward;
+import manager.svc.CityInsertService;
 import manager.svc.ManagerInsertProService;
 import manager.vo.CategoryBean;
 
-public class ManagerInsertProAction implements Action {
+public class CityInsertAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ActionForward forward = null;
-		
+
 		CategoryBean cb = new CategoryBean();
-		cb.setRegionName(request.getParameter("region_name"));
-		
-		ManagerInsertProService mips = new ManagerInsertProService();
-		boolean isInsertSuccess = mips.InsertCategory(cb);
-		
-		if(!isInsertSuccess) {
+		cb.setCityName(request.getParameter("city_name"));
+		cb.setCityRegionCode(Integer.parseInt(request.getParameter("region_code")));
+
+		CityInsertService cis = new CityInsertService();
+		boolean isInsertSuccess = cis.CityInsert(cb);
+
+		if (!isInsertSuccess) {
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			out.println("<script>");
-			out.println("alert('지역 등록 실패!')");
+			out.println("alert('도시 등록 실패!')");
 			out.println("history.back()");
 			out.println("</script>");
 		} else {
@@ -34,7 +36,7 @@ public class ManagerInsertProAction implements Action {
 //			forward.setRedirect(true);
 //			forward.setPath("ManagerMain.ma");
 		}
-		
+
 		return forward;
 	}
 
