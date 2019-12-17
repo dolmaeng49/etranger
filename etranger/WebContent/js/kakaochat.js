520d1464bbcd31ec2b310856dfd49626765a23f5
Bong <contactcsb119@gmail.com>
2019-12-17 14:05:15 +0900

    contact 초안

--- etranger/WebContent/js/kakaochat.js
+++ etranger/WebContent/js/kakaochat.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-    Kakao.init('6396a2950329721839be306c16858ef7');
+    Kakao.init('338d5b0fcaa6b7b36455221b4a956aa2');
     function chatChannel() {
       Kakao.Channel.chat({
         channelPublicId: '_tIzpT' // 카카오톡 채널 홈 URL에 명시된 id로 설정합니다.
